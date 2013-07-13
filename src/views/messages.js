422f88028b46a33bef3b87e2770874afac74f86d
Viktor Stanchev <me@viktorstanchev.com>
2013-07-12 23:35:21 -0700

    messages

--- src/views/messages.js
+++ src/views/messages.js
@@ -1,6 +1,15 @@
 (function(exports) {
   var MessagesView = Backbone.View.extend({
 
+    tagName: 'ui',
+
+    initialize: function() {
+      this.listenTo(this.collection, "change", this.render);
+    },
+
+    render: function() {
+
+    }
   });
 
   exports.MessagesView =  MessagesView;
