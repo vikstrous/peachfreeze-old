cd75c28ea3b261215c93359371aa2d528f0a3468
computerion <danielhu1234@gmail.com>
2013-07-13 04:10:45 -0700

    UI for profile edit

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,16 @@ $(function() {
 		  },200);
 	  }
 	);
+	
+	$('#peachfreeze #profile-button').click(
+		function(){
+			var oldNameText = $('#peachfreeze #profile-container .Name').text();
+			$('#peachfreeze #profile-container .Name').html("<input type='text' name='edit_name' id='edit_name_input' value='" + oldNameText + "'>");
+			var oldEpithetText = $('#peachfreeze #profile-container .Epithet').text().replace(/^\s+|\s+$/g, '');
+			$('#peachfreeze #profile-container .Epithet').html("<textarea  name='edit_epithet' id='edit_epithet_input'>" + oldEpithetText + "</textarea>");
+			var oldDescriptionText = $('#peachfreeze #profile-description').text().replace(/^\s+|\s+$/g, '');
+			$('#peachfreeze #profile-container #profile-description').html("<textarea name='edit_description' id='edit_description_input'>" + oldDescriptionText + "</textarea>");
+		});
 
 	var fingerPrint = $("#peachfreeze .FingerPrint").text().split(" ");
 	for(var i=0;i<fingerPrint.length;i++){
