ef7e331ab7e3e4e83a8b556d63a4ae0e733a22c5
Viktor Stanchev <me@viktorstanchev.com>
2013-07-12 23:36:39 -0700

    render users

--- src/views/users.js
+++ src/views/users.js
@@ -1,6 +1,15 @@
 (function(exports) {
   var UsersView = Backbone.View.extend({
 
+    tagName: 'ul',
+
+    initialize: function() {
+      this.listenTo(this.collection, "change", this.render);
+    },
+
+    render: function() {
+      console.log('render users', this.collection);
+    }
   });
 
   exports.UsersView = UsersView;
